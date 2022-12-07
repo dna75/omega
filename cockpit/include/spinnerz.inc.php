4655d84305155dfabc347d604ff16c7fc5ed2d2f
Nanne Dijkstra <info@spinnerz.nl>
2022-12-07 10:01:08 +0100

    wo  7 dec 2022 10:01:08 CET

--- cockpit/include/spinnerz.inc.php
+++ cockpit/include/spinnerz.inc.php
@@ -1,12 +1,12 @@
 <?php
-    $db_type = "mysql";
-    define('DB_HOST', "localhost");
-    define('DB_LOGIN', "omega");
-    define('DB_PASSWORD', "~1yk2Nd91");
-    define('DB_DB', "omega");
+$db_type = "mysql";
+define('DB_HOST', "localhost");
+define('DB_LOGIN', "omega");
+define('DB_PASSWORD', "~1yk2Nd91");
+define('DB_DB', "omega");
 
 $analytics = "";
 
 // Thumb size Gallery items website
 $customWidth = "400";
-$customHeight ="400";
+$customHeight = "400";
