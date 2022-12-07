2a2eb5b1f49761b1d8f8f91a9c620b931c21372f
Nanne Dijkstra <info@spinnerz.nl>
2022-12-07 10:29:47 +0100

    wo  7 dec 2022 10:29:47 CET

--- js/custom.js
+++ js/custom.js
@@ -105,47 +105,6 @@ $(".vehicle-data-select").change(function () {
 	return false;
 });
 
-// Initialize Datepicker
-//-------------------------------------------------------------------------------
-var nowTemp = new Date();
-var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-
-var checkin = $("#pick-up-date")
-	.datepicker({
-		onRender: function (date) {
-			return date.valueOf() < now.valueOf() ? "disabled" : "";
-		},
-	})
-	.on("changeDate", function (ev) {
-		if (ev.date.valueOf() > checkout.date.valueOf()) {
-			var newDate = new Date(ev.date);
-			newDate.setDate(newDate.getDate() + 1);
-			checkout.setValue(newDate);
-		}
-		checkin.hide();
-		$("#drop-off-date")[0].focus();
-	})
-	.data("datepicker");
-var checkout = $("#drop-off-date")
-	.datepicker({
-		onRender: function (date) {
-			return date.valueOf() <= checkin.date.valueOf() ? "disabled" : "";
-		},
-	})
-	.on("changeDate", function (ev) {
-		checkout.hide();
-	})
-	.data("datepicker");
-
-// Toggle Drop-Off Location
-//-------------------------------------------------------------------------------
-$(".input-group.drop-off").hide();
-$(".different-drop-off").on("click", function () {
-	$(".input-group.drop-off").toggle();
-	$(".autocomplete-suggestions").css("width", $(".pick-up .autocomplete-location").outerWidth());
-	return false;
-});
-
 // Scroll to Top Button
 //-------------------------------------------------------------------------------
 
